1017ed354761099bb3e0c62eede8e07e11afcc32
Wilson Montelo <wilson.montelo@hotmail.com>
2024-03-07 16:59:21 -0300

    feat: create user context

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,27 +3,33 @@ import "./globals.css";
 import { type_second } from "@/functions/fonts";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
+import userGet from "@/actions/user-get";
+import { UserContextProvider } from '@/context/user-context';
 
 export const metadata: Metadata = {
   title: "Dogs Next",
   description: "Rede social para cachorro",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+	const { data: user } = await userGet();
+	
   return (
     <html lang="pt-br">
       <body className={type_second.variable}>
-				<div className="App">
-				<Header />
-				<main className="AppBody">
-					{children}
-				</main>
-				<Footer />
-				</div>
+				<UserContextProvider user={user}>
+					<div className="App">
+					<Header />
+					<main className="AppBody">
+						{children}
+					</main>
+					<Footer />
+					</div>
+				</UserContextProvider>
 			</body>
     </html>
   );
